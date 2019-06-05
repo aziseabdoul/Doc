46d842a95224067418ba80fa1b8a3d9fca457c9e
aziseabdoul <49790808+aziseabdoul@users.noreply.github.com>
2019-06-05 14:49:16 +0200

    Add files via upload

---
+++ doc/doctor.js
@@ -0,0 +1,47 @@
+var e = document.getElementById('epaule');
+e.onmouseover = function() {
+  document.getElementById('texte').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('texte').style.display = 'none';
+}
+
+var e = document.getElementById('poignet');
+e.onmouseover = function() {
+  document.getElementById('texte2').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('texte2').style.display = 'none';
+}
+
+var e = document.getElementById('genou');
+e.onmouseover = function() {
+  document.getElementById('texte3').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('texte3').style.display = 'none';
+}
+
+var e = document.getElementById('coude');
+e.onmouseover = function() {
+  document.getElementById('texte4').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('texte4').style.display = 'none';
+}
+
+var e = document.getElementById('hanche');
+e.onmouseover = function() {
+  document.getElementById('texte5').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('texte5').style.display = 'none';
+}
+
+var e = document.getElementById('cheville');
+e.onmouseover = function() {
+  document.getElementById('texte6').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('texte6').style.display = 'none';
+}
